a63d9d4793b5a3322404123680e960c9886a07f2
youurt <ugurtigu@gmail.com>
2023-03-03 10:30:12 +0100

    Initial commit

---
+++ apps/tailwind-demo-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tb-tailwind-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tailwind-demo-app';
+}
