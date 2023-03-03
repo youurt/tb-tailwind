85cea081e0360aa62438abed6bfcf9fad83f3019
youurt <ugurtigu@gmail.com>
2023-03-03 13:01:37 +0100

    - removs scss file

--- apps/tailwind-demo-app/src/app/app.component.ts
+++ apps/tailwind-demo-app/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tb-tailwind-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tailwind-demo-app';
