37765f6c11c5083e862486d4f53e013eba827615
youurt <ugurtigu@gmail.com>
2023-03-03 12:36:32 +0100

    - add angular route

--- libs/common-feature/src/lib/common-feature.module.ts
+++ libs/common-feature/src/lib/common-feature.module.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AngularComponent } from './angular/angular.component';
 import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([{ path: '', component: MainComponent }]),
+    RouterModule.forChild([
+      { path: '', component: MainComponent },
+      {
+        path: 'angular',
+        component: AngularComponent,
+      },
+    ]),
   ],
 })
 export class CommonFeatureModule {}
--- libs/common-ui/src/lib/card/card.component.ts
+++ libs/common-ui/src/lib/card/card.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'tb-tailwind-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './card.component.html',
 })
 export class CardComponent {
   @Input() title?: string;
 
-  @Input() url?: string;
+  @Input() route?: string;
 }
